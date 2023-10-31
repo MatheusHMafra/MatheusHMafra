978619c7b1afd0d0d9028831700b6ec6b8cc1200
Matheus <127265280+MatheusHMafra@users.noreply.github.com>
2023-10-30 21:50:51 -0300

    oooooo projeto

---
+++ urlshortener/main.js
@@ -0,0 +1,6 @@
+/*
+Diminuir url:
+1) Converter url para base64 caso se o link conter https:// no começo
+2) Mostar url com o final diferente: ?link=(base64) caso se o link conter https:// no começo
+3) Se alguem entrar com o ?link=(base64) redirecionar ele caso se o link conter https:// no começo
+*/
